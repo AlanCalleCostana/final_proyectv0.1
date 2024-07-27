64ba565e45b37ef09eb37a86c2260c84e9d1af2c
Tony <calle.alan.130@gmail.com>
2024-07-27 18:49:09 -0400

    Se cambio el nombre del modelo de conexion a admin_model.php y se modifico en el autoload.php

---
+++ application/models/admin_model.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin_model extends CI_Model {
+
+	public function listusers()
+	{
+		$this->db->select('*');
+		$this->db->from('usuario');
+		return $this->db->get();  //Devuelve el resultado
+	}
+	public function agregarusuario($data)
+	{
+		$this->db->insert('usuario',$data);
+	}
+	public function validar($login,$password)
+	{
+		$this->db->select('*');
+		$this->db->from('usuario');
+		$this->db->where('usuario',$login);
+		$this->db->where('password',$password);
+		return $this->db->get(); //devuelve el resultado
+	}
+	public function listproducts()
+	{
+		$this->db->select('*');
+		$this->db->from('producto');
+		return $this->db->get();  //Devuelve el resultado
+	}
+
+	public function agregarlaptop($data)
+	{
+		$this->db->insert('usuario',$data);
+	}
+	public function eliminaruser($iduser)
+	{
+		$this->db->where('idusuario',$iduser);
+		$this->db->delete('usuario');
+	}
+
+}
