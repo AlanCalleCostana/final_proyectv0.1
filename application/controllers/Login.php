7e1360f36301d7e039fa1b155aa7c0457ddfcf60
Tony <calle.alan.130@gmail.com>
2024-07-30 16:18:29 -0400

    se modifico los archivos para el login, esta en una primera version, falta mejorar mucho

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,4 +7,57 @@ class Login extends CI_Controller {
 	{
 		$this->load->view('view_login');
 	}
+	public function agregaruserbd()
+	{
+		//lado izquierda coincide con el nombre de la base de datos, de las columnas
+		// el lado derecho es el name de el formulario
+		$data['nombre']=strtoupper($_POST['nombre']);// validacion para maysucula
+		$data['apellido']=strtoupper($_POST['apellido']);// validacion para maysucula
+		$data['email']=strtolower($_POST['email']);// validacion para minuscula
+		$data['telefono']=$_POST['telefono'];
+		$data['tipo']=$_POST['tipo'];
+		$data['usuario']=strtolower($_POST['user']);// validacion para maysucula
+		$data['password']=sha1($_POST['password']);
+		
+		$this->admin_model->agregarusuario($data);
+
+		redirect('login/index','refresh');
+	}
+	public function validarusuario()
+	{
+		$login=$_POST['usuario'];
+		$password=sha1($_POST['password']);
+
+		echo $login;
+		echo $password;
+		$consulta=$this->admin_model->validar($login,$password);
+
+		echo $consulta->num_rows();
+
+		if($consulta->num_rows()>0)
+		{
+			echo 'inicio de sesion';
+			//usuario valido
+			foreach($consulta->result() as $row)
+			{
+
+				$this->session->set_userdata('idusuario',$row->idusuario);
+				$this->session->set_userdata('usuario',$row->usuario);
+				//$this->session->set_userdata('tipo',$row->tipo);
+
+				redirect('PageAdmin/main','refresh');
+			}
+		}
+		else
+		{
+			//acceso incorrecto - volvemos al login
+			redirect('login/index','refresh');
+		}
+	}
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		redirect('login/index','refresh');
+	}
+
 }
--- application/views/view_login.php
+++ application/views/view_login.php
@@ -1,68 +1,94 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <script src="https://kit.fontawesome.com/64d58efce2.js"
-            crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous">
     </script>
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/stylelogin.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/stylelogin.css'); ?>">
 </head>
+
 <body>
     <div class="container">
         <div class="forms-container">
             <div class="signin-signup">
-                <form action="" class="sign-in-form">
-                    <h2 class="title">Iniciar Sesión</h2>
-                    <div class="input-field">
-                        <i class="fas fa-user"></i>
-                        <input type="text" placeholder="Nombre Usuario">
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-lock"></i>
-                        <input type="password" placeholder="Contraseña">
-                    </div>
-                    <input type="submit" value="Iniciar Sesion" class="btn solid">
-                    <p class="social-text">Redes Sociales</p>
-                    <div class="social-media">
-                        <a href="" class="social-icon">
-                            <i class="fab fa-facebook"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-google"></i>
-                        </a>
-                    </div>
-                </form>
+                <?php
+                $attributes = array('class' => 'sign-in-form');
+                echo form_open_multipart("login/validarusuario", $attributes);
+                ?>
+                <h2 class="title">Iniciar Sesión</h2>
+                <div class="input-field">
+                    <i class="fas fa-user"></i>
+                    <input type="text" name="usuario" placeholder="Nombre Usuario">
+                </div>
+                <div class="input-field">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" name="password" placeholder="Contraseña">
+                </div>
+                <input type="submit" value="Iniciar Sesion" class="btn solid">
+                <p class="social-text">Redes Sociales</p>
+                <div class="social-media">
+                    <a href="" class="social-icon">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-google"></i>
+                    </a>
+                </div>
+                <?php
+                echo form_close();
+                ?>
 
-
-                <form action="" class="sign-up-form">
-                    <h2 class="title">Registrarse</h2>
-                    <div class="input-field">
-                        <i class="fas fa-user"></i>
-                        <input type="text" placeholder="Nombre Usuario">
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-envelope"></i>
-                        <input type="password" placeholder="Email">
-                    </div>
-                    <input type="submit" value="registrarme" class="btn solid">
-                    <p class="social-text">Visita nuestras redes sociales</p>
-                    <div class="social-media">
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-facebook"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-google"></i>
-                        </a>
-                    </div>
-                </form>
+                <?php
+                $attributes = array('class' => 'sign-up-form');
+                echo form_open_multipart("login/agregaruserbd", $attributes);
+                ?>
+                <h2 class="title">Registrarse</h2>
+                <div class="input-field">
+                    <i class="fas fa-user"></i>
+                    <input type="text" name="nombre" placeholder="Nombre(s)" minlength="3" maxlength="20" required>
+                </div>
+                <div class="input-field">
+                    <i class="fas fa-user"></i>
+                    <input type="text" name="apellido" placeholder="Apellidos" minlength="3" maxlength="20" required>
+                </div>
+                <div class="input-field">
+                    <i class="fas fa-envelope"></i>
+                    <input type="email" name="email" placeholder="Email" required>
+                </div>
+                <div class="input-field">
+                    <i class="fas fa-mobile"></i>
+                    <input type="number" name="telefono" placeholder="Telefono" required>
+                </div>
+                <div class="input-field">
+                    <i class="fas fa-user"></i>
+                    <input type="text" name="user" placeholder="Nombre Usuario" minlength="4" maxlength="8" required>
+                </div>
+                <div class="input-field">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" name="password" placeholder="Contraseña" minlength="4" maxlength="8" equired>
+                </div>
+                <input type="submit" value="registrar" class="btn solid">
+                <p class="social-text">Visita nuestras redes sociales</p>
+                <div class="social-media">
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-google"></i>
+                    </a>
+                </div>
+                <?php
+                echo form_close();
+                ?>
             </div>
         </div>
 
@@ -86,6 +112,6 @@
             </div>
         </div>
     </div>
-    <script src="<?php echo base_url('assets/js/app.js');?>"></script>
+    <script src="<?php echo base_url('assets/js/app.js'); ?>"></script>
 </body>
 </html>
